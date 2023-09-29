0461108273b5499e1b2d6f6ab91626c64f094e1a
raviraj-laptop <144878065+raviraj-laptop@users.noreply.github.com>
2023-09-29 23:54:36 +0530

    push IIB Code

---
+++ Block_IIB.java
@@ -0,0 +1,25 @@
+package class_Demo;
+public class Block_IIB 
+{
+	static
+	{
+		System.out.println("I ma your SIB");  // SIB
+	}
+	{
+		System.out.println("I am Your IIB 1" );   //IIB
+	}
+	{
+		System.out.println("I am Your IIB 2" );   //IIB
+	}
+	{
+		System.out.println("I am Your IIB 3" );   //IIB
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("I am Main Method");   //Main method
+		new Block_IIB ();                         // Object for IIB
+
+	}
+
+}
